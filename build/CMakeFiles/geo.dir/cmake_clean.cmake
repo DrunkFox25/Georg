5eb31a5fe91d2c0cc994ad949f0e03c5d64b394f
Peter Aravin <112434193+DrunkFox25@users.noreply.github.com>
2026-08-31 01:43:52 +0000

    Testing Phase, No UI yet, will do QT later, for now fuck around have fun ig

---
+++ build/CMakeFiles/geo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geo.dir/GeorgEngine.cpp.o"
+  "CMakeFiles/geo.dir/GeorgEngine.cpp.o.d"
+  "geo"
+  "geo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
